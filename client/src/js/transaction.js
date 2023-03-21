e70f442c8372cb03924cf342c5a83fa10015f635
Antoniyo123 <riswantonr2015@gmail.com>
2023-03-21 12:37:04 +0700

    New Design for Event page (Revisi)

---
+++ client/src/js/transaction.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.btn__BUY').click(function(){
+        alert('Transaksi berhasil!'); 
+    });
+    $('.btn__Exchange').click(function(){
+        alert('Transaksi berhasil!'); 
+    });
+});
