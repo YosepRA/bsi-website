3eeb60727801d7aabbfdf528cbdbf20be42e3121
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2023-03-23 15:08:52 +0700

    ✨ Add countdown timer, popup system, and OTP UI.

---
+++ client/src/js/dialog.js
@@ -0,0 +1,42 @@
+class Dialog {
+  constructor() {
+    this.show = false;
+    this.node = null; // Full node.
+  }
+
+  render(node, dialogClass) {
+    if (!this.show) {
+      const dialog = document.querySelector('.dialog');
+      dialog.remove();
+
+      return undefined;
+    }
+
+    const dialog = document.createElement('div');
+    const dialogOverlay = document.createElement('div');
+
+    dialog.classList.add('dialog', dialogClass);
+    dialogOverlay.classList.add('dialog__overlay');
+
+    dialog.appendChild(dialogOverlay);
+    dialog.appendChild(node);
+
+    document.body.appendChild(dialog);
+  }
+
+  showDialog(node, dialogClass) {
+    this.show = true;
+    this.node = node;
+
+    this.render(node, dialogClass);
+  }
+
+  closeDialog() {
+    this.show = false;
+    this.node = null;
+
+    this.render();
+  }
+}
+
+export default Dialog;
