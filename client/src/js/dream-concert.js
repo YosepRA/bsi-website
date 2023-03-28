e4316196b737e531ba56215d51d9376d17534e36
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2023-03-28 16:40:01 +0700

    🎨 Mobile dialogs, Telegram button, and About BSI read more

--- client/src/js/dream-concert.js
+++ client/src/js/dream-concert.js
@@ -16,9 +16,25 @@ const checkTicketButton = document.querySelector(
 );
 const uidInputGuide = document.querySelector('.form-label__guide-btn--uid');
 const payeeInputGuide = document.querySelector('.form-label__guide-btn--payee');
+const aboutBSI = document.querySelector('.about-bsi');
+const aboutBSIShortBtn = document.querySelector(
+  '.about-bsi__col--info-short button',
+);
+const aboutBSILongBtn = document.querySelector(
+  '.about-bsi__col--info-long button',
+);
 
 function handleDreamConcertBuy() {
   window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+}
+
+function handleAboutBSIToggle(status) {
+  if (status) {
+    aboutBSI.classList.add('show');
+  } else {
+    aboutBSI.classList.remove('show');
+  }
+  aboutBSI.scrollIntoView();
 }
 
 function pricePoll(interval, ticket) {
@@ -46,6 +62,8 @@ function start() {
   checkTicketButton.addEventListener('click', ticket.showCheckTicket);
   uidInputGuide.addEventListener('click', ticket.showUIDGuide);
   payeeInputGuide.addEventListener('click', ticket.showPayeeCodeGuide);
+  aboutBSIShortBtn.addEventListener('click', () => handleAboutBSIToggle(true));
+  aboutBSILongBtn.addEventListener('click', () => handleAboutBSIToggle(false));
 
   // BSI price polling.
   const pollInterval = 60000;
